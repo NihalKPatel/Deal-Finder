d47796cf212576510e8d93b4df5cb02e676bdc2a
Nihal <70577135+NihalKPatel@users.noreply.github.com>
2021-04-17 21:53:15 +1200

    pages and base.html setup
    need to setup the basic template of boxes

--- deals/views.py
+++ deals/views.py
@@ -12,4 +12,32 @@ def profile(request):
 
 
 def shop(request):
-    return HttpResponse('shop')
+    return render(request, 'pages/shop.html')
+
+
+def dashboard(request):
+    return render(request, 'pages/dashboard.html')
+
+
+def budget(request):
+    return render(request, 'pages/budget.html')
+
+
+def browse(request):
+    return render(request, 'pages/browse.html')
+
+
+def categories(request):
+    return render(request, 'pages/categories.html')
+
+
+def compare_list(request):
+    return render(request, 'pages/compare_list.html')
+
+
+def shopping_list(request):
+    return render(request, 'pages/shopping_list.html')
+
+
+def map(request):
+    return render(request, 'pages/map.html')
