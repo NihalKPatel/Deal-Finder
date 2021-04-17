c3de75df2e2c6d80e69c268bf50151a36589bb79
Charlie Cumming <charliejc0806@gmail.com>
2021-04-18 00:33:43 +1200

    Implemented basic user authentication

--- dealfinder/urls.py
+++ dealfinder/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('deals/', include('deals.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
