59cd8708627e7149e6f04589d0d3529034dc3eb6
Micayla <micaylak12@gmail.com>
2021-04-18 15:33:59 +1200

    additional urls

--- deals/urls.py
+++ deals/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.dashboard, name='blog-home'),
+    path('', views.dashboard, name='deals-dashboard'),
+    path('profile/', views.profile, name='deals-profile'),
+    path('shop/', views.shopping, name='deals-shopping'),
+    path('budget/', views.budget, name='deals-budget'),
+    path('mappings/', views.mapping, name='deals-mapping'),
 ]
--- deals/views.py
+++ deals/views.py
@@ -4,3 +4,19 @@ from django.http import HttpResponse
 
 def dashboard(request):
     return HttpResponse('<h1>This is the dashboard</h1>')
+
+
+def profile(request):
+    return HttpResponse('<h1>This is the profile page</h1>')
+
+
+def shopping(request):
+    return HttpResponse('<h1>This is the shopping page</h1>')
+
+
+def budget(request):
+    return HttpResponse('<h1>This is the budget page</h1>')
+
+
+def mapping(request):
+    return HttpResponse('<h1>This is the maps page</h1>')
