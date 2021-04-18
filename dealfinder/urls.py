220dff3cfd89b9bf3107db7bc701e061fdc61e29
Charlie Cumming <charliejc0806@gmail.com>
2021-04-18 18:25:15 +1200

    -Add models to admin view -Add redirect from /deals to /

--- dealfinder/urls.py
+++ dealfinder/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('deals/', include('deals.urls')),
+    path('', RedirectView.as_view(url='deals/', permanent=True)),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
--- deals/admin.py
+++ deals/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
+from .models import Profile, Budget, Category, List, Product
 
+admin.site.register(Profile)
+admin.site.register(Budget)
+admin.site.register(Category)
+admin.site.register(List)
+admin.site.register(Product)
 
 
