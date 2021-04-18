55058bd51ae645389d6274160f56afc665441831
Micayla <micaylak12@gmail.com>
2021-04-18 15:25:10 +1200

    deals urls

--- deals/views.py
+++ deals/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def dashboard(request):
+    return HttpResponse('<h1>This is the dashboard</h1>')
