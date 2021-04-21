dfed65b00d43eb4e6bc7046bad528a9b3a60cf0a
Charlie Cumming <charliejc0806@gmail.com>
2021-04-21 16:18:34 +1200

    -Web scraping function for new world and pakinsave

--- dealfinder/urls.py
+++ dealfinder/urls.py
@@ -16,10 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
+from django.conf.urls.static import static
+from django.conf import settings
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('deals/', include('deals.urls')),
     path('', RedirectView.as_view(url='deals/', permanent=True)),
     path('accounts/', include('django.contrib.auth.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
