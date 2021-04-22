240f4331a80a6fe8878bfaa9f325f5fd7d8fba20
Micayla <micaylak12@gmail.com>
2021-04-22 23:59:46 +1200

    add image field

--- deals/models.py
+++ deals/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
     address = models.CharField(max_length=255)
 
 
--- users/models.py
+++ users/models.py
@@ -1,3 +1,3 @@
 from django.db import models
 
-# Create your models here.
+# Unsure if Profile database should be put in here or in deals/models.py
