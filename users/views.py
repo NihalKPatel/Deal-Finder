64b5da6433ea38e186898c20c47288eaa85f56d3
Micayla <micaylak12@gmail.com>
2021-04-22 23:30:20 +1200

    change create to register so form gets saved

--- users/views.py
+++ users/views.py
@@ -6,12 +6,12 @@ from .forms import UserRegisterForm
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}!')
             return redirect('index')
     else:
-        form = UserCreationForm()
+        form = UserRegisterForm()
     return render(request, 'registration/register.html', {'form': form})
