fb3fd366d9fe5a0f70104784678fcf15c1be0fe4
Nihal <70577135+NihalKPatel@users.noreply.github.com>
2021-05-16 03:49:31 +1200

    fixed the map(still needa import store location

--- deals/signals.py
+++ deals/signals.py
@@ -1,7 +1,7 @@
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
-from deals.models import Profile
+from deals.models import Profile, List
 
 
 # A user profile to be created for each new user
@@ -11,6 +11,7 @@ from deals.models import Profile
 def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
+        List.objects.create(user=instance)
 
 
 @receiver(post_save, sender=User)
