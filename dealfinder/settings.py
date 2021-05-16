2dd1e56962f169e339424a805191f32774bf225d
CharlieJC <charliejc0806@gmail.com>
2021-05-16 13:37:06 +1200

    Add vps IP to allowed hosts

--- dealfinder/settings.py
+++ dealfinder/settings.py
@@ -25,12 +25,11 @@ SECRET_KEY = '$q-buk*u(+%(f^p7k7rv%75zylaenm82hapfab=)22e-f1kw7k'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['139.99.133.173']
 
 # Application definition
 
 INSTALLED_APPS = [
-    'background_task',
     'deals.apps.DealsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
