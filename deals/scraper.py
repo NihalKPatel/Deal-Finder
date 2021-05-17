4efb49bfdb548641d63ccf7269d43f70960e08f1
CharlieJC <charliejc0806@gmail.com>
2021-05-18 09:07:57 +1200

    Remove redundant code

--- deals/scraper.py
+++ deals/scraper.py
@@ -19,7 +19,6 @@ class Store:
         'Accept-Language': 'en',
     }
     url = None
-    data = {}
     type = None
 
     # returns a 2-tuple (name, price)
@@ -38,13 +37,8 @@ class NewWorld(Store):
 
     def scrape_product_data(self, page=1):
         name_and_price = []
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.128 Safari/537.36',
-            'Accept-Language': 'en',
-        }
 
-        r = requests.get(f'{self.url}&pg={page}', headers=headers)
-        print(f'{self.url}&pg={page}')
+        r = requests.get(f'{self.url}&pg={page}', headers=self.headers)
 
         soup = BeautifulSoup(r.text, "lxml")
 
@@ -63,7 +57,6 @@ class NewWorld(Store):
     def save_to_db(self):
         Product.objects.filter(location=self.name).delete()
         for i in range(1, 21):
-            print("Scraping page " + str(i) + " from new world")
             name_price_tuples = self.scrape_product_data(i)
             print(len(name_price_tuples))
             for item in name_price_tuples:
@@ -80,12 +73,8 @@ class ComputerLounge(Store):
 
     def scrape_product_data(self, page=1):
         name_and_price = []
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.128 Safari/537.36',
-            'Accept-Language': 'en',
-        }
 
-        r = requests.get(self.url, headers=headers)
+        r = requests.get(self.url, headers=self.headers)
 
         soup = BeautifulSoup(r.text, "lxml")
 
@@ -105,54 +94,6 @@ class ComputerLounge(Store):
 
 
 all_stores = [NewWorld(), ComputerLounge()]
-# def get_single_item_data_amazon(url):
-#     headers = {
-#         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.128 Safari/537.36',
-#         'Accept-Language': 'en',
-#     }
-#
-#     r = requests.get(url, headers=headers)
-#
-#     soup = BeautifulSoup(r.text, "lxml")
-#
-#     name = soup.select_one(selector="#productTitle").getText()
-#     name = name.strip()
-#
-#     price = soup.select_one(selector="#priceblock_ourprice").getText()
-#     price = float(price[1:])
-#
-#     return name, price
-
-
-# def get_item_search_data_nw(url):
-#     name_and_price = []
-#     headers = {
-#         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.128 Safari/537.36',
-#         'Accept-Language': 'en',
-#     }
-#
-#     r = requests.get(url, headers=headers)
-#
-#     soup = BeautifulSoup(r.text, "lxml")
-#
-#     products = soup.select(selector=".js-product-card-footer")
-#     product_data = []
-#
-#     for product in products:
-#         if "data-options" in product.attrs:
-#             product_data.append(json.loads(product["data-options"]))
-#
-#     for item in product_data:
-#         name_and_price.append((item['productName'], item['ProductDetails']['PricePerItem']))
-#     return name_and_price
-
-
-# def scrape_all_products():
-#     Product.objects.all().delete()
-#     for i in range(1, 51):
-#         print("Scraping page " + str(i) + " from new world")
-#         for item in get_item_search_data_nw(new_world_url + '&pg=' + str(i)):
-#             Product.objects.create(name=item[0], price=item[1], link='https://www.newworld.co.nz/', location='New World')
 
 def main():
     pass
