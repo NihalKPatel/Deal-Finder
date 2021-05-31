637c22a0a04f9a50c3cb4ea78853b735db482f95
Nihal <70577135+NihalKPatel@users.noreply.github.com>
2021-06-01 01:28:10 +1200

    testing

--- deals/tests/test_views.py
+++ deals/tests/test_views.py
@@ -1,5 +1,10 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 from django.urls import reverse
+from django.test import TestCase
+from django.test import Client
+from ..forms import UserRegisterForm
+
 
 class DashboardViewTest(TestCase):
 
@@ -18,3 +23,21 @@ class DashboardViewTest(TestCase):
         response = self.client.get(reverse('dashboard'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'pages/dashboard.html')
+
+
+class DetailsViewTest(TestCase):
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get('/deals/details')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse('details'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('details'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'pages/details.html')
+
+
+
