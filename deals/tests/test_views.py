100e6d488ffcb6f584b554411708f325a84553b4
dyingCSstudent <vmd6421@autuni.ac.nz>
2021-06-01 21:09:28 +1200

    -tests

--- deals/tests/test_views.py
+++ deals/tests/test_views.py
@@ -36,4 +36,20 @@ class DetailsViewTest(TestCase):
         self.assertTemplateUsed(response, 'pages/details.html')
 
 
+class BudgetCalculatorTest(TestCase):
 
+    # test that the BudgetCalculator view can be accessed via the following url
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get('/deals/budget_calculator')
+        self.assertEqual(response.status_code, 200)
+
+    # test that the BudgetCalculator view can be accessed via the following name
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse('budget_calculator'))
+        self.assertEqual(response.status_code, 200)
+
+    # test that the BudgetCalculator view is using the correct template
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('budget_calculator'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'pages/budget_calculator.html')
