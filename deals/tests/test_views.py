ed2532bf1fbc4b3ebbf30f5fcf9aa2dd7e6a854a
SURFACE\Patricia <patriciavirgenaguilar@gmail.com>
2021-06-01 21:26:33 +1200

    TDD test for FAQ
    testing pages

--- deals/tests/test_views.py
+++ deals/tests/test_views.py
@@ -1,20 +1,20 @@
 from django.test import TestCase
 from django.urls import reverse
 
-class DashboardViewTest(TestCase):
+class faq(TestCase):
 
     # test that the dashboard view can be acessed via the following url
-    def test_view_url_exists_at_desired_location(self):
-        response = self.client.get('/deals/dashboard')
+    def test_view_url_exists_at_desired_location_faq(self):
+        response = self.client.get('/deals/FAQ')
         self.assertEqual(response.status_code, 200)
 
     # test that the dashboard view can be acessed via the following name
-    def test_view_url_accessible_by_name(self):
-        response = self.client.get(reverse('dashboard'))
+    def test_view_url_accessible_by_name_faq(self):
+        response = self.client.get(reverse('faq'))
         self.assertEqual(response.status_code, 200)
 
     # test that the dashboard view is using the correct template
-    def test_view_uses_correct_template(self):
-        response = self.client.get(reverse('dashboard'))
+    def test_view_uses_correct_template_faq(self):
+        response = self.client.get(reverse('FAQ'))
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'pages/dashboard.html')
+        self.assertTemplateUsed(response, 'pages/faq.html')
